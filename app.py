67379490416f58fcd31a4a88513bf3e40bb26567
@johnya0 <anoopjohny2000@gmail.com>
2023-06-09 16:42:19 +0200

    commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+from imdb import IMDb
+
+app = Flask(__name__)
+ia = IMDb()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search')
+def search_movies():
+    query = request.args.get('query')
+    if query:
+        movies = ia.search_movie(query)
+        return render_template('result.html', movies=movies)
+    else:
+        return render_template('result.html', movies=[])
+
+@app.route('/movie/<imdb_id>')
+def movie_details(imdb_id):
+    movie = ia.get_movie(imdb_id)
+    return render_template('details.html', movie=movie)
+
+if __name__ == '__main__':
+    app.run(debug=True)
