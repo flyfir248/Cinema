5293aedc4472cabeeb922027ef32d332b1165e76
@johnya0 <anoopjohny2000@gmail.com>
2023-06-11 11:12:06 +0200

    commit

--- app.py
+++ app.py
@@ -1,32 +1,51 @@
 from flask import Flask, render_template, request
 from imdb import IMDb
+from wikipedia import summary, exceptions
 
 app = Flask(__name__)
 ia = IMDb()
 
+# Route for home page
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-@app.route('/search')
-def search_movies():
-    query = request.args.get('query')
-    if query:
-        movies = ia.search_movie(query)
-        return render_template('result.html', movies=movies)
-    else:
-        return render_template('result.html', movies=[])
+# Route for receiving user preferences and providing movie suggestions
+@app.route('/suggest', methods=['POST'])
+def suggest():
+    # Get user preferences from the form
+    genre = request.form.get('genre')
+    min_rating = float(request.form.get('min_rating'))
 
-@app.route('/genre/<genre_name>')
-def genre_movies(genre_name):
-    genre_id = ia.get_keyword(genre_name)
-    movies = ia.get_keyword(genre_id, results=20)
-    return render_template('result.html', movies=movies)
+    # Search for movies based on user preferences
+    movies = ia.search_movie(genre)
+    suggestions = []
+    for movie in movies:
+        ia.update(movie)
+        if movie.get('rating') and movie['rating'] >= min_rating:
+            try:
+                movie_summary = summary(movie['title'], sentences=2)
+                suggestions.append({
+                    'title': movie['title'],
+                    'rating': movie['rating'],
+                    'image': movie.get('full-size cover url'),
+                    'summary': movie_summary
+                })
+            except exceptions.DisambiguationError:
+                # Handle DisambiguationError by skipping the movie
+                continue
+            except exceptions.PageError:
+                # Handle PageError by adding a fallback message
+                movie_summary = 'No data available'
+                suggestions.append({
+                    'title': movie['title'],
+                    'rating': movie['rating'],
+                    'image': movie.get('full-size cover url'),
+                    'summary': movie_summary
+                })
 
-@app.route('/movie/<imdb_id>')
-def movie_details(imdb_id):
-    movie = ia.get_movie(imdb_id)
-    return render_template('details.html', movie=movie)
+    # Render the suggestions template with the movie suggestions
+    return render_template('suggestions.html', suggestions=suggestions)
 
 if __name__ == '__main__':
     app.run(debug=True)
